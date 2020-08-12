b31c304da77582fffa155afa83cc72d07c27c17b
Carlos Vinicius Ananias <carlos.oliveira69@fatec.sp.gov.br>
2020-08-11 22:50:48 -0300

    first test with jest

---
+++ index.js
@@ -0,0 +1,17 @@
+function concatenar(letter1, letter2) {
+    return letter1 + letter2;
+}
+
+function subtrair(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplicar(mult1, mult2) {
+    return mult1 * mult2;
+}
+
+function dividir(div1, div2) {
+    return div1 / div2;
+}
+
+module.exports = { concatenar, subtrair, multiplicar, dividir }
